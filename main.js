5df1115a2c25dff5cec8349967c1745bfbd8fbd3
장유리 <jyuri1114@naver.com>
2024-11-26 23:25:19 +0900

    [add] merge test

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-//스와이퍼 모달 등장
-document.getElementById("album").addEventListener("click", function () {
-  console.log("??");
-  document.querySelector(".modal").classList.add("show-modal");
-
-  document.body.classList.add("modal-open");
-});
-
-document.getElementById("close-btn").addEventListener("click", function () {
-  console.log("모달 닫기");
-  document.querySelector(".modal").classList.remove("show-modal");
-  document.body.classList.remove("modal-open");
-});
-
 //스크롤바 내리면 area-2 애니메이션 시작.
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 500) {
@@ -23,27 +9,67 @@ window.addEventListener("scroll", function () {
   }
 });
 
-//이미지
-const photoData = [
-  { image: "img/img02.jpg" },
-  { image: "img/img03.jpg" },
-  { image: "img/img04.jpg" },
-  { image: "img/img05.jpg" },
-  { image: "img/img06.jpg" },
-  { image: "img/img07.jpg" },
-  { image: "img/img08.jpg" },
-  { image: "img/img09.jpg" },
-  { image: "img/img10.jpg" },
-  { image: "img/img02.jpg" },
-  { image: "img/img02.jpg" },
-];
+// window.onload = function () {
+//   // 지도 초기화
+//   const mapContainer = document.getElementById("map"); // 지도를 표시할 div
+//   const mapOption = {
+//     center: new kakao.maps.LatLng(37.5355, 127.0944), // 강변 웨딩스퀘어의 좌표
+//     level: 3, // 지도의 확대 레벨
+//   };
 
-const wrapper = document.querySelector(".swiper-wrapper");
+//   // 지도 생성
+//   const map = new kakao.maps.Map(mapContainer, mapOption);
 
-photoData.forEach((photo) => {
-  wrapper.innerHTML += `
-        <div class="swiper-slide">
-            <img src="${photo.image}" alt="" />
-        </div>
-    `;
+//   // 마커 추가
+//   const markerPosition = new kakao.maps.LatLng(37.5355, 127.0944); // 마커의 위치
+//   const marker = new kakao.maps.Marker({
+//     position: markerPosition,
+//   });
+//   marker.setMap(map);
+
+//   // 인포윈도우 추가 (강변 웨딩스퀘어 정보)
+//   const infowindow = new kakao.maps.InfoWindow({
+//     content:
+//       '<div style="padding:5px; font-size:14px;">강변 웨딩스퀘어<br>3층 루시드홀</div>',
+//   });
+//   infowindow.open(map, marker);
+// };
+
+//계좌번호
+document.getElementById("chang").addEventListener("click", function (e) {
+  if (e.target == document.querySelector(".title-top")) {
+    console.log(e.target);
+    document.querySelector(".chang-detail").classList.toggle("show-detail");
+  }
+});
+
+document.getElementById("yuri").addEventListener("click", function (e) {
+  if (e.target == document.querySelector(".title-yuri")) {
+    console.log(e.target);
+    document.querySelector(".yuri-detail").classList.toggle("show-detail");
+  }
+});
+
+// 복사버튼
+document.querySelectorAll(".row button").forEach((button) => {
+  button.addEventListener("click", function () {
+    // 현재 버튼과 동일한 row 안의 p.bank를 찾기
+    const bankTextElement = this.closest(".row").querySelector(".bank");
+
+    // span을 제외한 텍스트 추출
+    const bankText = bankTextElement.childNodes[0].textContent.trim();
+
+    // 클립보드에 복사
+    navigator.clipboard
+      .writeText(bankText)
+      .then(() => {
+        alert(`복사되었습니다: ${bankText}`);
+      })
+      .catch((err) => {
+        console.error("복사에 실패했습니다:", err);
+      });
+
+    this.classList.add("copied");
+    setTimeout(() => this.classList.remove("copied"), 2000);
+  });
 });
